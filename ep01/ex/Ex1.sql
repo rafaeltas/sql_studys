dee050b76025465333f3db2fb328bd1900c12907
Rafael Tas <rafaeltas@gmail.com>
2025-05-07 10:26:43 -0300

    Resolução do primeiro exercício do ep01.

--- ep01/ex/Ex1.sql
+++ ep01/ex/Ex1.sql
@@ -1,5 +1,28 @@
 -- Ex1: Quantos produtos temos da categoria 'artes'?
 -- select * from tb_products
-select count(product_category_name) as "Número de produtos da categoria Artes"
-from "tb_products"
+-- select count(product_category_name)
+-- as 'Número de produtos da categoria Artes' 
+-- from "tb_products"
+-- where product_category_name = 'artes'
+
+-- Correção
+-- select count(*) as 'Número_de_Produtos' -- IDENTIFICADOR SEM ESPAÇOS
+
+-- Uso do operador "distinct"
+-------------------------------
+-- select count(distinct product_id)
+--
+-- from tb_products
+--
+-- where product_category_name = 'artes'
+
+-------------------------------
+select
+    count(*) as 'qtde_linhas',
+    count(product_id) as 'qtde_produtos',
+    count(distinct product_id) as 'qtde_produtos_distintos',
+    count(distinct product_category_name) as 'qtde_categorias_distintas'
+
+from tb_products
+
 where product_category_name = 'artes'
