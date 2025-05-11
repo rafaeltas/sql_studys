f35f7cd1d3e6250cb6483f7b3129a6859d731beb
Rafael Tas <rafaeltas@gmail.com>
2025-05-11 11:45:10 -0300

    Resolução ex1 do ep04 de SQL

---
+++ ep04/ex/Ex1_solo.sql
@@ -0,0 +1,19 @@
+-- ### Ex1
+-- Qual o valor total de receita gerada total de cada estado?
+-- Considere a base completa, com apenas pedidos entregues
+SELECT
+    t3.customer_state AS 'estado',
+    SUM(t2.payment_value) AS 'receita_por_estado'
+
+FROM tb_orders AS t1
+
+LEFT JOIN tb_order_payments AS t2
+    ON t1.order_id = t2.order_id
+
+LEFT JOIN tb_customers AS t3
+    ON t1.customer_id = t3.customer_id
+
+WHERE t1.order_status = 'delivered'
+
+GROUP BY t3.customer_state
+ORDER BY SUM(t2.payment_value) DESC
--- ep04/ex/Ex3.sql
+++
@@ -1,4 +0,0 @@
--- ### Ex3
-
--- Qual o peso médio dos produtos vendidos por sellers de cada estado?
--- Considere apenas o ano de 2017 e pedidos entregues nesta análise.
